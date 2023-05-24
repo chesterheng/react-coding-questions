8af696b0d7921e51ae1391bbc88e6c7b4f83f509
Chester Heng <chester.heng@gmail.com>
2023-05-24 10:19:51 +0800

    Holy Grail

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Counter from "./components/Counter";
 import ContactForm from "./components/ContactForm";
+import HolyGrail from "./components/HolyGrail";
 
 function App() {
   return (
     <>
-      <ContactForm />
+      <HolyGrail />
     </>
   );
 }
---
+++ src/components/HolyGrail.tsx
@@ -0,0 +1,15 @@
+import "./HolyGrail.css";
+
+export default function App() {
+  return (
+    <>
+      <header>Header</header>
+      <div className="columns">
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </>
+  );
+}
